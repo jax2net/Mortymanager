b12b73e4971f2f481f10d5aea98a9e7788dcf840
Jake Santos <jakesantos@tuta.io>
2020-04-07 23:34:56 -0700

    established connection to API

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+// app.js by Jacob Santos
+
 // The JS file where calls to handle API calls / DOM manipulation
+let requestURL = 'https://rickandmortyapi.com/api/character';
+let data; // holds the JSON Object
+// we are getting the JSON object
+fetch(requestURL)
+	.then(
+	    function (response) {
+		if (response.status !== 200) {
+		    console.log('Theres a problem : ' + response.status);
+		    return;
+		}
+		// look at response
+		response.json().then((json) => {
+		    console.log(json);
+		    data = json;
+		});
+	    }
+	).catch((error) => {
+	    console.log("Fetch error : ", err);
+	});
